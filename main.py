95870c86a06d2769fcfdecb4a72931ffce4102b2
Brandon Ceasar <bobbopkins@gmail.com>
2018-07-25 09:12:43 -0700

    Version 9:
    message stuff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from google.appengine.api import memcache
+from google.appengine.ext import ndb
 from google.appengine.api import users
 
 import datetime
@@ -12,22 +13,25 @@ jinja_current_directory = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
-#OBJECTS
-# class Message(object):
-#     def __init__(self, email, text):
-#         self.email = email
-#         self.text = text
-#         self.timestamp = datetime.datetime.now()
-#     def to_dict(self):
-#         result = {
-#             "text": self.text,
-#             "timestamp": self.timestamp.strftime("%Y-%m-%d %H-%M-%S")
-#         }
-#         if len(self.email) > 10:
-#             result['email'] = self.email.split("@", 1)[0]
-#         else:
-#             result['email'] = self.email
-#         return result
+class Model(ndb.Model):
+    genre = ndb.StringProperty()
+    text = ndb.TextProperty()
+
+class Message(object):
+    def __init__(self, text):
+        self.text = text
+        self.timestamp = datetime.datetime.now()
+    def to_dict(self):
+        result = {
+            "text": self.text,
+            "timestamp": self.timestamp.strftime("%Y-%m-%d %H-%M-%S")
+        }
+        # if len(self.email) > 10:
+        #     result['email'] = self.email.split("@", 1)[0]
+        # else:
+        #     result['email'] = self.email
+        return result
+
 
 #HANDLERS
 class GetMainPageHandler(webapp2.RequestHandler):
@@ -43,11 +47,11 @@ class GetMainPageHandler(webapp2.RequestHandler):
             'rnb': 'R&B!',
         }
         self.response.out.write(main_page.render(params))
-        print(users.get_current_user())
 
 class GetGenreHandler(webapp2.RequestHandler):
     def get(self):
         genre = self.request.get("genre")
+        print(genre)
         if genre == "gospel":
             genre_page = jinja_current_directory.get_template("templates/gospelpage.html")
         if genre == "hiphop":
@@ -61,73 +65,33 @@ class GetGenreHandler(webapp2.RequestHandler):
         if genre == "rnb":
             genre_page = jinja_current_directory.get_template("templates/rnbpage.html")
 
-
-        self.response.out.write(genre_page.render())
-
-class SetUserDataHandler(webapp2.RequestHandler):
-    def dispatch(self):
-
-
+            self.response.out.write(genre_page.render())
 
 class AddMessageHandler(webapp2.RequestHandler):
     def dispatch(self):
         result = {}
-        email = get_current_user_email()
-        if email:
-            msg_text = self.request.get("text")
-            if len(msg_text) > 500:
-                result["error"] = "Message is too long."
-            elif not msg_text.strip():
-                result["error"] = "Message is empty."
-            else:
-                messages = memcache.get("messages")
-                if not messages:
-                    messages = []
-                msg = Message(email, msg_text)
-                messages.append(msg)
-                memcache.set("messages", messages)
-                result["OK"] = True
+        # email = get_current_user_email()
+        text = self.request.get("home")
+        # Model.genre = self.request.get(genre)
+        if len(text) > 500:
+            result["error"] = "Message is too long."
+        elif not text.strip():
+            result["error"] = "Message is empty."
         else:
-            result["error"] = "User is not logged in."
-        send_json(self, result)
-#
-# class GetLoginUrlHandler(webapp2.RequestHandler):
-#     def dispatch(self):
-#         result = {
-#             "url": users.create_login_url("/")
-#         }
-#         send_json(self, result)
-#
-# class GetLogoutUrlHandler(webapp2.RequestHandler):
-#     def dispatch(self):
-#         result = {
-#             "url": users.create_logout_url("/")
-#         }
-#         send_json(self, result)
-#
-# class GetMessageHandler(webapp2.RequestHandler):
-#     def dispatch(self):
-#         email = get_current_user_email()
-#         result = {}
-#         if email:
-#             result["messages"] = []
-#             messages = memcache.get("messages")
-#             if messages:
-#                 for message in messages:
-#                     result["messages"].append(message.to_dict())
-#         else:
-#             result["error"] = "User is not logged in"
-#         send_json(self, result)
-#
-# class GetUserHandler(webapp2.RequestHandler):
-#     def dispatch(self):
-#         email = get_current_user_email()
-#         result = {}
-#         if email:
-#             result["user"] = email
-#         else:
-#             result["error"] = "User is not logged in"
-#         send_json(self, result)
+            messages = memcache.get("messages")
+            if not messages:
+                messages = []
+            msg = Message(text)
+            messages.append(msg)
+            memcache.set("messages", messages)
+            result["OK"] = True
+
+        m = Model(text=text)
+        m.put()
+
+        # else:
+        #     result["error"] = "User is not logged in."
+
 #
 # #DEFS
 # def get_current_user_email():
@@ -136,15 +100,13 @@ class AddMessageHandler(webapp2.RequestHandler):
 #         return current_user.email()
 #     else:
 #         return None
-#
-def send_json(request_handler, props):
-    request_handler.response.content_type = "application/json"
-    request_handler.response.out.write(json.dumps(props))
+
 
 
 #MAPPING
 app = webapp2.WSGIApplication([
     ("/", GetMainPageHandler),
+    ("/add", AddMessageHandler),
     ("/genre", GetGenreHandler),
     # ("/setUserData", SetUserDataHandler)
     # ("/gospel", GospelHandler),
