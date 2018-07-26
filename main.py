a76926531129b5f0a6b79570d2887acd93304218
Brandon Ceasar <bobbopkins@gmail.com>
2018-07-26 12:13:30 -0700

    Version 11:
    added about page

--- main.py
+++ main.py
@@ -50,6 +50,11 @@ class GetMainPageHandler(webapp2.RequestHandler):
         }
         self.response.out.write(main_page.render(params))
 
+class GetAboutPageHandler(webapp2.RequestHandler):
+    def get(self):
+        main_page = jinja_current_directory.get_template("templates/aboutPage.html")
+        self.response.out.write(main_page.render())
+
 class GetGenreHandler(webapp2.RequestHandler):
     def get(self):
         genre = self.request.get("genre")
@@ -153,6 +158,66 @@ class GetMessageHandler(webapp2.RequestHandler):
         # else:
         #     result["error"] = "User is not logged in"
 
+class User(ndb.Model):
+  """CssiUser stores information about a logged-in user.
+
+  The AppEngine users api stores just a couple of pieces of
+  info about logged-in users: a unique id and their email address.
+
+  If you want to store more info (e.g. their real name, high score,
+  preferences, etc, you need to create a Datastore model like this
+  example).
+  """
+  first_name = ndb.StringProperty()
+  last_name = ndb.StringProperty()
+
+class GetUserHandler(webapp2.RequestHandler):
+  def get(self):
+    user = users.get_current_user()
+    # If the user is logged in...
+    if user:
+      email_address = user.nickname()
+      the_user = User.get_by_id(user.user_id())
+      signout_link_html = '<a href="%s">sign out</a>' % (
+          users.create_logout_url('/login'))
+      # If the user has previously been to our site, we greet them!
+      if the_user:
+        self.response.write('''
+            Welcome %s %s (%s)! <br> %s <br>''' % (
+              the_user.first_name,
+              the_user.last_name,
+              email_address,
+              signout_link_html))
+      # If the user hasn't been to our site, we ask them to sign up
+      else:
+        self.response.write('''
+            Welcome to our site, %s!  Please sign up! <br>
+            <form method="post" action="/">
+            <input type="text" name="first_name">
+            <input type="text" name="last_name">
+            <input type="submit">
+            </form><br> %s <br>
+            ''' % (email_address, signout_link_html))
+    # Otherwise, the user isn't logged in!
+    else:
+      self.response.write('''
+        Please log in to use our site! <br>
+        <a href="%s">Sign in</a>''' % (
+          users.create_login_url('/login')))
+
+  def post(self):
+    user = users.get_current_user()
+    if not user:
+      # You shouldn't be able to get here without being logged in
+      self.error(500)
+      return
+    the_user = User(
+        first_name=self.request.get('first_name'),
+        last_name=self.request.get('last_name'),
+        id=user.user_id())
+    the_user.put()
+    self.response.write('Thanks for signing up, %s!' %
+        the_user.first_name)
 
 #
 # #DEFS
@@ -172,8 +237,8 @@ app = webapp2.WSGIApplication([
     ("/genre", GetGenreHandler),
     ("/messages", GetMessageHandler),
     # ("/setUserData", SetUserDataHandler)
-    # ("/gospel", GospelHandler),
-    # ("/login", GetLoginUrlHandler),
+    ("/about", GetAboutPageHandler),
+    ("/login", GetUserHandler),
     # ("/logout", GetLogoutUrlHandler),
     # ("/user", GetUserHandler)
 ], debug=True)
